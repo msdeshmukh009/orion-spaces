80d47ce63c657fb082111d9e3777b02968156348
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 01:31:53 +0530

    feat: useTheme hook created

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context";
+
+const useTheme = () => {
+  const { currentTheme, setCurrentTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    if (currentTheme === "dark") {
+      setCurrentTheme("");
+      localStorage.setItem("spaces-theme", "");
+    } else {
+      setCurrentTheme("dark");
+      localStorage.setItem("spaces-theme", "dark");
+    }
+  };
+
+  return { currentTheme, setCurrentTheme, toggleTheme };
+};
+
+export { useTheme };
