7852db1cd5b1e28571baffac076b3a5e25786b3c
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 21:03:11 +0530

    feat: bookmark page created

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { Login, SignUp } from "./authentication";
 export { Home } from "./home/Home";
 export { PostDetail } from "./post-detail/PostDetail";
 export { Profile } from "./profile/Profile";
+export { Bookmark } from "./bookmark/Bookmark";
--- src/routes/NavigationRoutes.jsx
+++ src/routes/NavigationRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Login, SignUp, Home, PostDetail, Profile } from "../pages";
+import { Login, SignUp, Home, PostDetail, Profile, Bookmark } from "../pages";
 
 const NavigationRoutes = () => {
   return (
@@ -9,6 +9,7 @@ const NavigationRoutes = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/post-detail" element={<PostDetail />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/bookmark" element={<Bookmark />} />
     </Routes>
   );
 };
