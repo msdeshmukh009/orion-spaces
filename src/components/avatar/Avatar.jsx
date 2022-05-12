38714567bf1ff6eb220dca3eaaa10a88fc8211fb
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 17:10:01 +0530

    feat: avatar component extracted

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,11 @@
+const Avatar = () => {
+  return (
+    <img
+      className="rounded-[50%]"
+      src="https://avatars.dicebear.com/api/avataaars/your-custom-seed52.svg"
+      alt="user-name"
+    />
+  );
+};
+
+export { Avatar };
