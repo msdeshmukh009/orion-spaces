33250709baa7eee0aad91f85df5eb357dcc06fc5
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 17:11:14 +0530

    refactor: exported avatar, comment

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,6 @@ export { Base } from "./base/Base";
 export { SearchBar } from "./search-bar/SearchBar";
 export { MobileTopBar } from "./mobile-top-bar/MobileTopBar";
 export { MobileBottomBar } from "./mobile-bottom-bar/MobileBottomBar";
+export { Avatar } from "./avatar/Avatar";
+export { PostCtaBar } from "./post-cta-bar/PostCtaBar";
+export { Comment } from "./comment/Comment";
