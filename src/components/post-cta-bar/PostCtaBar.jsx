6b0fa802350cab912963b0042a91a5e7bc00fc0c
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-13 00:00:03 +0530

    refactor: options for posts

--- src/components/post-cta-bar/PostCtaBar.jsx
+++ src/components/post-cta-bar/PostCtaBar.jsx
@@ -13,25 +13,25 @@ const PostCtaBar = () => {
   return (
     <div className="flex justify-between gap-4 w-full p-2">
       <button
-        className="flex gap-2 text-xl rounded-sm p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
+        className="flex gap-2 text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
         title="Like"
       >
         <IoMdHeartEmpty /> <span className="text-sm">2</span>
       </button>
       <button
-        className="flex gap-2 text-xl rounded-sm p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
+        className="flex gap-2 text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
         title="Reply"
       >
         <MdOutlineModeComment /> <span className="text-sm">2</span>
       </button>
       <button
-        className="text-xl rounded-sm p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
+        className="text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
         title="Bookmark"
       >
         {pathname === "/bookmark" ? <MdOutlineBookmark /> : <MdOutlineBookmarkBorder />}
       </button>
       <button
-        className="text-xl rounded-sm p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
+        className="text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
         title="Share"
       >
         <MdOutlineShare />
--- src/components/user-post/UserPost.jsx
+++ src/components/user-post/UserPost.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { BsThreeDots } from "react-icons/bs";
+import { MdOutlineEdit, MdOutlineDeleteOutline } from "react-icons/md";
 import { Avatar } from "../avatar/Avatar";
 import { PostCtaBar } from "../post-cta-bar/PostCtaBar";
 
 const UserPost = () => {
+  const [showPostOptions, setShowPostOptions] = useState(false);
+  const { pathname } = useLocation();
+
   return (
-    <div className="flex gap-2 border-b-2 border-secondary-color-50 py-2 hover:bg-secondary-color-50 dark:border-secondary-color-dm-50 dark:hover:bg-secondary-color-dm-50">
+    <div className="flex rounded-md border-b-2 border-secondary-color-50 py-2 hover:bg-secondary-color-50 dark:border-secondary-color-dm-50 dark:hover:bg-secondary-color-dm-50">
       <div className="basis-14 shrink-0">
         <Avatar />
       </div>
       <div className="flex flex-col gap-2">
         <div className="flex flex-col">
-          <div className="flex gap-1">
+          <div className="flex gap-1 px-2">
             <Link to="/profile" className="font-semibold">
               The cool User
             </Link>
@@ -18,13 +24,35 @@ const UserPost = () => {
               @theUserName
             </Link>
           </div>
-          <Link to="/post-detail">
+          <Link to="/post-detail" className="px-2 mt-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos aliquam consequatur
             cumque, nemo aperiam ratione tempora sequi obcaecati maiores.
           </Link>
         </div>
         <PostCtaBar />
       </div>
+      {pathname === "/profile" && (
+        <div className="relative">
+          <button
+            onClick={() => setShowPostOptions(prevState => !prevState)}
+            className="p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100 rounded-[30rem]"
+          >
+            <BsThreeDots />
+          </button>
+          <div
+            className={`${
+              showPostOptions ? "flex" : "hidden"
+            } absolute justify-center flex-col gap-1 top-5 right-4 w-32 text-sm shadow-lg p-1 rounded-md bg-secondary-color-100 dark:bg-secondary-color-dm-100`}
+          >
+            <div className="flex gap-1 items-center hover:bg-secondary-color-50 dark:hover:bg-secondary-color-dm-50 cursor-pointer p-1 rounded-md">
+              <MdOutlineEdit /> <span className="flex-1">Edit Post</span>
+            </div>
+            <div className="flex gap-1 items-center hover:text-red-color hover:bg-secondary-color-50 dark:hover:bg-secondary-color-dm-50 cursor-pointer p-1 rounded-md">
+              <MdOutlineDeleteOutline /> <span className="flex-1">Delete Post</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
