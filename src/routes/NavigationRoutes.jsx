28dfccee1b00f710874c0febad5f846195c23d55
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 17:12:14 +0530

    feat: route for post detail page created

--- src/routes/NavigationRoutes.jsx
+++ src/routes/NavigationRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Login, SignUp, Home } from "../pages";
+import { Login, SignUp, Home, PostDetail } from "../pages";
 
 const NavigationRoutes = () => {
   return (
@@ -7,6 +7,7 @@ const NavigationRoutes = () => {
       <Route path="/" element={<Login />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/post-detail" element={<PostDetail />} />
     </Routes>
   );
 };
