7042f557f786cd4c986483d0270474b8c6358470
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 20:31:46 +0530

    style: padding adjusted and heading removed

--- src/components/mobile-top-bar/MobileTopBar.jsx
+++ src/components/mobile-top-bar/MobileTopBar.jsx
@@ -2,7 +2,7 @@ import { SearchBar } from "../search-bar/SearchBar";
 
 const MobileTopBar = () => {
   return (
-    <div className="sm:hidden sticky p-2 flex gap-2 items-center top-0 w-full bg-background-color dark:bg-background-color-dm z-10">
+    <div className="sm:hidden sticky py-2 px-4 flex gap-2 items-center top-0 w-full bg-background-color dark:bg-background-color-dm z-10">
       <div className="basis-12">
         <a href="/">
           <img
@@ -12,7 +12,7 @@ const MobileTopBar = () => {
           />
         </a>
       </div>
-      <h1 className="text-xl">Home</h1>
+
       <div className="w-3/4 m-auto">
         <SearchBar />
       </div>
