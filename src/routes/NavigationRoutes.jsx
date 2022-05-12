c08523290b5d167b238cee5e3313fafb0eee8ea1
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-12 20:34:47 +0530

    feat: created route for profile page

--- src/routes/NavigationRoutes.jsx
+++ src/routes/NavigationRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Login, SignUp, Home, PostDetail } from "../pages";
+import { Login, SignUp, Home, PostDetail, Profile } from "../pages";
 
 const NavigationRoutes = () => {
   return (
@@ -8,6 +8,7 @@ const NavigationRoutes = () => {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/home" element={<Home />} />
       <Route path="/post-detail" element={<PostDetail />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
