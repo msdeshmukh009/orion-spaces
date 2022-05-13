1fdbb6b5665b7a8b312a870342bde75922ff47d9
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-13 05:33:33 +0530

    refactor: profile link added

--- src/components/mobile-top-bar/MobileTopBar.jsx
+++ src/components/mobile-top-bar/MobileTopBar.jsx
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 import { SearchBar } from "../search-bar/SearchBar";
 
 const MobileTopBar = () => {
   return (
     <div className="sm:hidden sticky py-2 px-4 flex gap-2 items-center top-0 w-full bg-background-color dark:bg-background-color-dm z-10">
       <div className="basis-12">
-        <a href="/">
+        <Link to="/profile">
           <img
             className="rounded-[50%] w-full"
             src="https://avatars.dicebear.com/api/avataaars/your-custom-seed52.svg"
             alt="user-name"
           />
-        </a>
+        </Link>
       </div>
 
       <div className="w-3/4 m-auto">
