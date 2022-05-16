fc03b0abb75cabd586f056baa90cacfb45070def
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-16 19:48:58 +0530

    refactor: added like count

--- src/components/post-cta-bar/PostCtaBar.jsx
+++ src/components/post-cta-bar/PostCtaBar.jsx
@@ -7,30 +7,50 @@ import {
 } from "react-icons/md";
 import { IoMdHeartEmpty } from "react-icons/io";
 
-const PostCtaBar = () => {
+const PostCtaBar = ({ likeCount }) => {
   const { pathname } = useLocation();
 
   return (
     <div className="flex justify-between gap-4 w-full p-2">
+      <div className="flex items-center">
+        <button
+          onClick={e => {
+            e.stopPropagation();
+          }}
+          className="flex gap-2 text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
+          title="Like"
+        >
+          <IoMdHeartEmpty />
+        </button>
+        <span className="text-sm p-2 rounded-[50%]">{likeCount ? likeCount : null}</span>
+      </div>
+
+      <div className="flex">
+        <button
+          onClick={e => {
+            e.stopPropagation();
+          }}
+          className="flex gap-2 text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
+          title="Reply"
+        >
+          <MdOutlineModeComment />
+        </button>
+        <span className="text-sm p-2 rounded-[50%]">{}</span>
+      </div>
+
       <button
-        className="flex gap-2 text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
-        title="Like"
-      >
-        <IoMdHeartEmpty /> <span className="text-sm">2</span>
-      </button>
-      <button
-        className="flex gap-2 text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
-        title="Reply"
-      >
-        <MdOutlineModeComment /> <span className="text-sm">2</span>
-      </button>
-      <button
+        onClick={e => {
+          e.stopPropagation();
+        }}
         className="text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
         title="Bookmark"
       >
         {pathname === "/bookmark" ? <MdOutlineBookmark /> : <MdOutlineBookmarkBorder />}
       </button>
       <button
+        onClick={e => {
+          e.stopPropagation();
+        }}
         className="text-xl rounded-[30rem] p-2 hover:bg-secondary-color-100 dark:hover:bg-secondary-color-dm-100"
         title="Share"
       >
