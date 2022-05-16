e2144d0eb08c6a0a57383f8aa93e62228d15aa7c
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-16 19:49:29 +0530

    refactor: added edit post modal

--- src/components/base/Base.jsx
+++ src/components/base/Base.jsx
@@ -1,26 +1,21 @@
 import "./base.css";
-import { MdOutlineEdit, MdOutlineClose } from "react-icons/md";
-import { Sidebar, SuggestionsSidebar, MobileTopBar, MobileBottomBar } from "../index";
-import { Modal } from "../modal/Modal";
-import { TextEditor } from "../text-editor/TextEditor";
-import { usePostModal } from "../../hooks";
+import { MdOutlineEdit } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { openPostModal } from "../../features/post/postSlice";
+import {
+  Sidebar,
+  SuggestionsSidebar,
+  MobileTopBar,
+  MobileBottomBar,
+  EditPostModal,
+} from "../index";
 
 const Base = ({ children }) => {
-  const { showPostModal, setShowPostModal } = usePostModal();
+  const dispatch = useDispatch();
 
   return (
     <div className="bg-background-color dark:bg-background-color-dm text-text-color dark:text-text-color-dm">
-      <Modal showModal={showPostModal}>
-        <div className="bg-secondary-color-50 dark:bg-secondary-color-dm-100 w-4/5 xl:w-1/3  p-2 rounded-md">
-          <div className="w-full flex justify-end">
-            <button className="p-2" onClick={() => setShowPostModal(false)}>
-              <MdOutlineClose className="close-btn" />
-            </button>
-          </div>
-
-          <TextEditor />
-        </div>
-      </Modal>
+      <EditPostModal />
 
       <MobileTopBar />
 
@@ -37,7 +32,7 @@ const Base = ({ children }) => {
 
         <div className="flex justify-center fixed bottom-12 text-2xl right-2 sm:hidden">
           <button
-            onClick={() => setShowPostModal(true)}
+            onClick={() => dispatch(openPostModal())}
             className="bg-primary-color-100 hover:bg-primary-color-200 active:bg-primary-color-100 text-secondary-color-100 flex justify-center items-center rounded-[50%] w-16 h-16 p-2 text-center "
           >
             <MdOutlineEdit />
